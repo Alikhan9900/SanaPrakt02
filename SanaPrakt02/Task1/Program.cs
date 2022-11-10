840d8564a7863997a8101b30b398690763301d0e
Аліхан Бекіров <alikhanbekirov@gmail.com>
2022-11-10 13:50:40 +0200

    Change text

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello");
+Console.WriteLine("My");
+Console.WriteLine("Friend");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+
